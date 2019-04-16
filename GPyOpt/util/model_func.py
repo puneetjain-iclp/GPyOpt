9d646351b30ad40491b752251559b5ea728953a4
Puneet Jain <puneet.jain@iclployalty.com>
2019-04-16 15:43:05 +0530

    adding custom files in the package

---
+++ GPyOpt/util/model_func.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def model_predict(x):
+    a = np.copy(x)
+    print(x)
+    a[x <= -5] = .8
+    a[(x > -5) & (x <= 0)] = .6
+    a[(x >0) & (x <= 5 )] = .4
+    a[x > 5]= .1
+    return a
