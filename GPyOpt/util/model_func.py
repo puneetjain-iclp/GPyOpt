061843cf1eb856baebb0bf176259107a8f947143
Puneet Jain <puneet.jain@iclployalty.com>
2019-07-22 16:22:49 +0530

    Adding the changes to the model func definition

--- GPyOpt/util/model_func.py
+++ GPyOpt/util/model_func.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import jit
 import boto3
 import botocore
 import lightgbm as lgb
@@ -12,19 +13,38 @@ import lightgbm as lgb
 # #     a[x > 5]= .1
 # #     return a
 
-# @jit
 def model_predict(x,mod_data,model):
-    n = np.empty((x.shape[0],x.shape[1]))
+    elasticity_vars =["ipo_elasticity_factor_bronze","ipo_elasticity_factor_silver","ipo_elasticity_factor_gold"]
     col = mod_data.columns
+    n = np.empty((x.shape[0],x.shape[1]))
     for counter in range(0,x.shape[0]):
-        if 'IPO_optimise_factor_Bronze' in col:
-            mod_data['IPO_optimise_factor_Bronze'] = x[counter,:,0]
-        if 'IPO_optimise_factor_Silver' in col:
-            mod_data['IPO_optimise_factor_Silver'] = x[counter,:,1]
-        if 'IPO_optimise_factor_Gold' in col:
-            mod_data['IPO_optimise_factor_Gold'] = x[counter,:,2]
+        mod_data = create_features(mod_data,counter,x,elasticity_vars,col)        
         n[counter,:] =  model.predict_proba(mod_data.values)[:,1]
+        # print(model.predict_proba(mod_data.values)[:,1].sum()/len(model.predict_proba(mod_data.values)[:,1]))
     return n
 
 
+def create_features(mod_data,counter,x,elasticity_vars,col):
+    if 'ipo_elasticity_factor_bronze' in col:
+        mod_data['ipo_elasticity_factor_bronze'] = x[counter,:,0]
+    if 'ipo_elasticity_factor_silver' in col:
+        mod_data['ipo_elasticity_factor_silver'] = x[counter,:,1]
+    if 'ipo_elasticity_factor_gold' in col:
+        mod_data['ipo_elasticity_factor_gold'] = x[counter,:,2]
+    if 'b_s_diff' in col:
+        mod_data['b_s_diff'] = x[counter,:,0] - \
+            x[counter,:,1]
+    if 'b_g_diff' in col:
+        mod_data['b_g_diff'] = x[counter,:,0] - \
+            x[counter,:,2]
+    if 's_g_diff' in col:
+        mod_data['s_g_diff'] = x[counter,:,1] - \
+            x[counter,:,2]
+    # if 'elas_mean' in col:
+    #     mod_data['elas_mean'] = mod_data[elasticity_vars].mean(axis=1)
+    # if 'elas_sum' in col:
+    #     mod_data['elas_sum'] = mod_data[elasticity_vars].sum(axis=1)
+    # if 'elas_stdev' in col:
+    #     mod_data['elas_stdev'] = mod_data[elasticity_vars].std(axis=1)
+    return mod_data
 
